7ca6d627d199ca8d989b8d2d6d9050e74356570a
rxsss <rakhmanmagametov@gmail.com>
2022-05-02 21:02:17 +0600

    First commit of app

---
+++ UnsplashPremium/Presentation Layer/Search Page/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  UnsplashPremium
+//
+//  Created by Abdurakhman on 02.05.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
